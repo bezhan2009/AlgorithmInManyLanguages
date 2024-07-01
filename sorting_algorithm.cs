a5f9f25c1bbe960124ec10ccba7760429f75b4e6
Bejan <157696306+bezhan2009@users.noreply.github.com>
2024-07-02 01:03:31 +0300

    Create sorting_algorithm.cs

---
+++ sorting_algorithm.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        BubbleSort(arr);
+        Console.Write("Sorted array: ");
+        PrintArray(arr);
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var num in arr)
+            Console.Write(num + " ");
+        Console.WriteLine();
+    }
+}
