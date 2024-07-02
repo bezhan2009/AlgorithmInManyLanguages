9cd9ab795961c7328e6449c827a86f274bfd150a
Bejan <157696306+bezhan2009@users.noreply.github.com>
2024-07-02 21:04:38 +0300

    Create sorting_algorithm(Handler-C).c

---
+++ c/sorting_algorithm(Handler-C).c
@@ -0,0 +1,26 @@
+// Пример сортировки пузырьком на Handel-C
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Пример использования
+#include <stdio.h>
+int main() {
+    int arr[] = {5, 3, 8, 4, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
