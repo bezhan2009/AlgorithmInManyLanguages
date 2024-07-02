0660deb0752f70ae89edb40e616530bb0fcc95aa
Bejan <157696306+bezhan2009@users.noreply.github.com>
2024-07-02 20:42:25 +0300

    Create sorting_algorithm(AJAX).js

---
+++ j/sorting_algorithm(AJAX).js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+    var n = arr.length;
+    for (var i = 0; i < n - 1; i++) {
+        for (var j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// Пример использования
+var arr = [5, 3, 8, 4, 2];
+console.log(bubbleSort(arr));
